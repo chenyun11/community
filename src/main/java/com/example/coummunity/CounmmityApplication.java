525f5cd6d30745f21e2dcac789bd8772bfcd2c03
chenyun11 <1424590853@qq.com>
2020-01-10 22:46:50 +0800

    first commit

---
+++ src/main/java/com/example/coummunity/CounmmityApplication.java
@@ -0,0 +1,13 @@
+package com.example.coummunity;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CounmmityApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CounmmityApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/coummunity/counmmity/CounmmityApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.coummunity.counmmity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CounmmityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
